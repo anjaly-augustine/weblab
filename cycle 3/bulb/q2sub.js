15e9da6751db18b8f06b0207912526a4838b7019
anjaly-augustine <anjalyaugustine35s@gmail.com>
2024-10-30 11:51:43 +0530

    Add files via upload

---
+++ cycle 3/bulb/q2sub.js
@@ -0,0 +1,8 @@
+document.getElementById('onButton').addEventListener('click',function()
+{
+	document.getElementById('lightBulb').src='bulbon.jpg';
+});
+document.getElementById('offButton').addEventListener('click',function()
+{
+	document.getElementById('lightBulb').src='bulboff.jpg';
+});
